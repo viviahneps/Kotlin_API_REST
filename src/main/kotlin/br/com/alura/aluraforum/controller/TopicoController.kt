4d7906a4527e49b2e4689528f7e5fc2977b3bc2e
viviahneps <vivizinha.vne@gmail.com>
2024-02-17 19:40:34 -0300

    inclusao :swagger

--- src/main/kotlin/br/com/alura/aluraforum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/aluraforum/controller/TopicoController.kt
@@ -4,6 +4,7 @@ import br.com.alura.aluraforum.DTOS.TopicoAtlzForm
 import br.com.alura.aluraforum.DTOS.TopicoForm
 import br.com.alura.aluraforum.DTOS.TopicoView
 import br.com.alura.aluraforum.Service.TopicoService
+import io.swagger.v3.oas.annotations.Operation
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -13,18 +14,19 @@ import org.springframework.web.util.UriComponentsBuilder
 @RestController
 @RequestMapping ("/topicos")
 class TopicoController(private val service: TopicoService) {
+   @Operation(summary = "Obtem lista de tópicos")
    @GetMapping
   fun listar(): List<TopicoView> {
        return service.listar()
    }
-
+    @Operation(summary = "Obtem lista de tópicos por ID")
     @GetMapping ("/{id}")
     //Path indica que o parametro vem no endpoint
    fun buscaId(@PathVariable id: Long): TopicoView{
    return service.buscarporId(id)
   }
 
-
+    @Operation(summary = "Efetua cadastro de tópico")
     @PostMapping
     //RequestBody indica que o parametro vem pelo corpo da mensagem
     fun cadastrar(@RequestBody @Valid dto:TopicoForm,
@@ -34,12 +36,13 @@ class TopicoController(private val service: TopicoService) {
         val uri =uriBuilder.path("/topicos/${topicoView.id}").build().toUri()
         return ResponseEntity.created(uri).body(topicoView)
     }
-
+    @Operation(summary = "Efetua atualização de tópico")
     @PutMapping
     fun atualilzar(@RequestBody @Valid form: TopicoAtlzForm) : ResponseEntity<TopicoView>{
      val topicoView = service.atualizar(form)
         return ResponseEntity.ok(topicoView)
     }
+    @Operation(summary = "Efetua exclusão de tópico")
     @DeleteMapping ("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deletar(@PathVariable  id: Long){
